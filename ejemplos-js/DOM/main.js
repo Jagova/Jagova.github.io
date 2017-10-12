fd5a2341282b6c7644d4770099a83ccffb538335
Jagova <pacojagova@gmail.com>
2017-10-12 11:50:49 -0500

    delegacion de eventos, agregar mas cajas y eliminarlas con el click

--- ejemplos-js/DOM/main.js
+++ ejemplos-js/DOM/main.js
@@ -1,10 +1,18 @@
 var btnAgregar = document.getElementById("btnAgregar");
 //btnAgregar.onclick = agregaCaja;
+var padre = document.getElementById("contenedor");
 
-btnAgregar.addEventListener('click', function anonima() {
+//delegacion de eventos
+padre.addEventListener('click', function(evento){
+    var hijo = evento.target;
+    if(hijo != this)  //validar que no se hizo click en el mismo contenedor
+        this.removeChild(hijo);
+});
+
+btnAgregar.addEventListener('click', function anonima() {   
     var entrada = document.getElementById('entrada');
     var contenido = entrada.value;
-    if(contenido != '')
+    if (contenido != '')
         agregaCaja(contenido);
 });
 
@@ -12,12 +20,16 @@ function agregaCaja(texto) {
     //var cajas = document.getElementsByTagName('div');
     var nuevo = document.createElement('div');
     var texto = document.createTextNode(texto);
+    
     nuevo.appendChild(texto);
     nuevo.setAttribute("class", "caja");
     var seccion = document.getElementsByTagName('section');
     seccion[0].appendChild(nuevo);
+
+   
+    
 }
 
-function anonima() {
 
-}
+
+
