353ceec79356a04520c34f2953b229faef5be5e3
Jagova <pacojagova@gmail.com>
2017-10-18 15:42:38 -0500

    Avanze de programa de registro

--- ejemplos-js/Registro/registro.js
+++ ejemplos-js/Registro/registro.js
@@ -2,17 +2,17 @@ var formulario = document.getElementById('registro');
 var mensaje = document.getElementById('mensaje');
 
 formulario.addEventListener('submit', function(evento){
-    if(!verificaNombre() ||!verificaContraseña() ||!verificaTerminos())
+    if(!verificaNombre() || !verificaContraseña() || !verificaTerminos())
         evento.preventDefault();
     else
         {  
-            mensaje.innerHTML = 'Oli prro';
+            mensaje.innerHTML = 'Tu registro fue realizado con éxito';
         }
 });
 
 function verificaNombre()
 {
-    var expReg = /a-z]/i;
+    var expReg = /[a-z]/i;
     
 
     if(expReg.test(formulario.user.value.charAt(0)))
@@ -30,10 +30,29 @@ function verificaNombre()
 
 function verificaContraseña()
 {
+    var letras = /[a-z]/i;
+    var numeros = /[0-9]/i;
+
+    if(formulario.pwd.value.length<8)
+        {
+            mensaje.innerHTML = 'La contraseña debe contener al menos 8 caracteres';
+            return false;
+        }
+    if(formulario.pwd.value != formulario.rpwd.value)
+    {
+        mensaje.innerHTML = 'La contraseñas no coincien';
+        return false;
+    }
+
     return true;
 }
 
 function verificaTerminos()
 {
+    if(formulario.terminos.value == false)
+    {
+        mesanje.innerHTML = 'Debes aceptar los terminos y condiciones';
+        return false;
+    }
     return true;
 }
